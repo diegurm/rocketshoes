a419da152e0b9a9aac8e28564059ff1ba651378c
Diego Rodrigues <diegurm@hotmail.com>
2020-03-18 23:32:54 -0300

    feat - Criado estilização da listagem de produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+          alt="Tênis Tryon Tauri Masculino"
+        />
+        <strong>Tênis Tryon Tauri Masculino</strong>
+        <span>R$ 129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+          alt="Tênis Tryon Tauri Masculino"
+        />
+        <strong>Tênis Tryon Tauri Masculino</strong>
+        <span>R$ 129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+          alt="Tênis Tryon Tauri Masculino"
+        />
+        <strong>Tênis Tryon Tauri Masculino</strong>
+        <span>R$ 129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+          alt="Tênis Tryon Tauri Masculino"
+        />
+        <strong>Tênis Tryon Tauri Masculino</strong>
+        <span>R$ 129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+          alt="Tênis Tryon Tauri Masculino"
+        />
+        <strong>Tênis Tryon Tauri Masculino</strong>
+        <span>R$ 129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+          alt="Tênis Tryon Tauri Masculino"
+        />
+        <strong>Tênis Tryon Tauri Masculino</strong>
+        <span>R$ 129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
