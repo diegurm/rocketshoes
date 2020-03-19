55cad163d1a06c603540022d4efdeb798950494a
Diego Rodrigues <diegurm@hotmail.com>
2020-03-19 00:18:41 -0300

    feat - Buscando produtos da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/formart';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
-          alt="Tênis Tryon Tauri Masculino"
-        />
-        <strong>Tênis Tryon Tauri Masculino</strong>
-        <span>R$ 129.99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
-          alt="Tênis Tryon Tauri Masculino"
-        />
-        <strong>Tênis Tryon Tauri Masculino</strong>
-        <span>R$ 129.99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
-          alt="Tênis Tryon Tauri Masculino"
-        />
-        <strong>Tênis Tryon Tauri Masculino</strong>
-        <span>R$ 129.99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
-          alt="Tênis Tryon Tauri Masculino"
-        />
-        <strong>Tênis Tryon Tauri Masculino</strong>
-        <span>R$ 129.99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
-          alt="Tênis Tryon Tauri Masculino"
-        />
-        <strong>Tênis Tryon Tauri Masculino</strong>
-        <span>R$ 129.99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
-          alt="Tênis Tryon Tauri Masculino"
-        />
-        <strong>Tênis Tryon Tauri Masculino</strong>
-        <span>R$ 129.99</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
+
+    const response = await api.get('/products');
+    const products = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
+    this.setState({
+      products,
+      loading: false
+    });
+  }
+
+  render() {
+    const { loading, products } = this.state;
+
+    return (
+      <ProductList>
+        {loading ? (
+          <h1>Carregando...</h1>
+        ) : (
+          products.map(product => (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
+              <button type="button">
+                <div>
+                  <MdAddShoppingCart size={16} color="#FFF" /> 3
+                </div>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          ))
+        )}
+      </ProductList>
+    );
+  }
 }
