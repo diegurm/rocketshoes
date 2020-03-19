bfe2bccc2175d2280907fa493d075566df403489
Diego Rodrigues <diegurm@hotmail.com>
2020-03-18 23:58:34 -0300

    feat - Adicionado estilização no carinho de compra

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <h1>Cart</h1>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-tryon-tauri-masculino/80/C89-0221-180/C89-0221-180_zoom2.jpg?ts=1567615393&ims=326x"
+                alt="Tênis Tryon Tauri Masculino"
+              />
+            </td>
+            <td>
+              <strong>Tênis Tryon Tauri Masculino</strong>
+              <span>R$ 129.99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="text" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$233,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong> R$993,00</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
