7f622a1dc41d4a954423ac6b74dfe3b1d8f27eb6
Diego Rodrigues <diegurm@hotmail.com>
2020-03-23 22:58:39 -0300

    feat: Calculando quantidade dos itens do carinho

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -8,3 +8,11 @@ export function addToCart(product) {
 export function removeFromCart(id) {
   return { type: '@cart/REMOVER', id };
 }
+
+export function updateAmount(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT',
+    id,
+    amount
+  };
+}
